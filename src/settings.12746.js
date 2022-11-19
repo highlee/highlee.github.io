53f886b67a0f7787817279f00cbd8d8d270854e8
highlee <highlee98@gmail.com>
2022-11-19 12:37:28 +0800

    first commit

---
+++ src/settings.12746.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scene/MainMenu.fire",orientation:"",jsList:[],bundleVers:{internal:"3c089",resources:"e1175",main:"95736"}};
